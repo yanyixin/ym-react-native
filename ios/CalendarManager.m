892a2ecfce47182b6bd210a3f7a93e214526a5f7
yanyixin <ymfighting1@gmail.com>
2017-11-15 23:08:56 +0800

    update: update eslint

--- ios/CalendarManager.m
+++ ios/CalendarManager.m
@@ -13,9 +13,9 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSString *)ISO8601DateString)
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
 {
-  NSDate *date = [RCTConvert NSDate:ISO8601DateString];
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
 @end
